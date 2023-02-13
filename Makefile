519c402323e01870125486aacb390e67940ba7ea
Oleg Gromov <mail@oleggromov.com>
2023-02-13 02:50:38 +0000

    Update makefile and json.c

--- Makefile
+++ Makefile
@@ -1,9 +1,12 @@
 CFLAGS=-Wall -Wextra -pedantic -std=c99 -g
 
-json: json.c tokenize.o util.o
+json: json.c dynarray.o hashmap.o tokenize.o parse.o util.o murmur3_32.o
 	$(CC) $(CFLAGS) -o $@.out $^
 
 hashmap: hashmap.c murmur3_32.o
+	$(CC) $(CFLAGS) -o $@.out $^
+
+dynarray_test: dynarray_test.c dynarray.o util.o
 	$(CC) $(CFLAGS) -o $@.out $^
 
 %.o: %.c
--- json.c
+++ json.c
@@ -1,8 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "tokenize.h"
 
-char* SAMPLE_INPUT = "{\"string_1\": \"a not so long string value\",\"object_1\": {\"number_float\": 3.14,\"number_int\": 327681,, \"negative_number\": -0.231}\n}";
+#include "hashmap.h"
+#include "tokenize.h"
+#include "parse.h"
+
+// TODO fix "falsenull" or "truefalsefalse" cases
+char* SAMPLE_INPUT = "{\"string_1\": \"a not so long string value\",\"object_1\": {\"number_float\": 3.14,\"number_int\": 327681, \"negative_number\": -0.231}\n, \"bool_true\": true, \"bool_false\": false, \"number\": 2.88, \"inner beauty\": [1, 2, 3, null]}";
+
+char* NO_ARRAY = "{\"string_1\": \"a not so long string value\",\"object_1\": {\"number_float\": 3.14,\"number_int\": 327681, \"negative_number\": -0.231}\n, \"bool_true\": true, \"bool_false\": false, \"number\": 2.88, },";
+
+char* SIMPLE = "{\"key_number\": 25.0303030303, \"key_string\": \"string\", \"boolean\": false, \"long\": 9200000 }";
+
+char* SIMPLE_ARRAY = "[1,2,null,false,\"an example string\"]";
 
 // 1. read file
 // 2. split it into tokens
@@ -11,18 +21,24 @@ char* SAMPLE_INPUT = "{\"string_1\": \"a not so long string value\",\"object_1\"
 // 5. write it back to fs
 int main()
 {
-  struct TokenList* tokens = tokenize(SAMPLE_INPUT);
+  token_list_t* tokens = tokenize(NO_ARRAY);
 
   printf("\n\ntoken list length = %llu\n", tokens->length);
   if (tokens->length > 0) {
     printf("TOKENS:\n\n");
-    for (unsigned long long i = 0; i < tokens->length; i++) {
+    for (token_length_t i = 0; i < tokens->length; i++) {
       print_token_list(&tokens->tokens[i]);
     }
     printf("\n");
   }
 
-  free_token_list(tokens);
+  node_t* root = parse(tokens);
+  printf("node root pointer = %p\n", root);
+
+  // char* serialized = serialize(root);
+
+  // printf("serialized:\n");
+  // printf("%s\n", serialized);
 
   return EXIT_SUCCESS;
 }
