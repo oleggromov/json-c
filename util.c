d6e39a5022a50364636aa710a2962776325cdeb4
Oleg Gromov <mail@oleggromov.com>
2023-02-13 23:36:56 +0000

    Make die() a proper wrapper for printf

--- util.c
+++ util.c
@@ -1,7 +1,15 @@
 #include "util.h"
 
-void die(char* message)
+void die(char* message, ...)
 {
-  fprintf(stderr, "%s\n", message);
+  char format[1024] = {0};
+  strcat(&format, message);
+  strcat(&format, "\n");
+
+  va_list args;
+  va_start(args, message);
+  vfprintf(stderr, format, args);
+  va_end(args);
+
   exit(EXIT_FAILURE);
 }
--- util.h
+++ util.h
@@ -1,9 +1,11 @@
 #ifndef UTIL_H
 #define UTIL_H
 
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-void die(char* message);
+void die(char* message, ...);
 
 #endif // UTIL_H
