7d3715b15df9d07c25f122e149a549c4f4a6917a
Oleg Gromov <mail@oleggromov.com>
2023-02-15 21:37:48 +0000

    Add recursive serialization

--- serialize.c
+++ serialize.c
@@ -6,7 +6,6 @@ typedef struct {
   unsigned long buf_len;
 } serialize_state_t;
 
-
 static const size_t BUF_INCREMENT = 1024;
 static void append_buf_str(serialize_state_t* state, char* str, unsigned long str_len)
 {
@@ -26,72 +25,203 @@ static void append_buf_str(serialize_state_t* state, char* str, unsigned long st
 }
 
 
-char* serialize(node_t* node)
+static void step(serialize_state_t* state, node_t* node, int depth)
 {
+  size_t arr_len = -1;
+  size_t obj_key_count = -1;
+  char** obj_keys = NULL;
+  char number_buf[1024] = {0};
+
+  switch (node->type) {
+    case NodeObject:
+      append_buf_str(state, "{", 1);
+
+      obj_key_count = hashmap_count_keys(node->value);
+      obj_keys = hashmap_get_keys(node->value);
+
+      for (size_t i = 0; i < obj_key_count; i++) {
+        append_buf_str(state, "\"", 1);
+        append_buf_str(state, obj_keys[i], strlen(obj_keys[i]));
+        append_buf_str(state, "\"", 1);
+        append_buf_str(state, ": ", 2);
+
+        step(state, hashmap_get(node->value, obj_keys[i]), depth + 1);
+
+        if (i < obj_key_count - 1) {
+          append_buf_str(state, ", " , 2);
+        }
+      }
+
+      append_buf_str(state, "}", 1);
+      break;
+
+    case NodeArray:
+      append_buf_str(state, "[", 1);
+
+      arr_len = ((dynarray_t*) node->value)->len;
+      // printf("depth=%d, type=%d, value=%p, len=%d\n", depth, node->type, node->value, arr_len);
+
+      for (size_t i = 0; i < arr_len; i++) {
+        node_t* child = dynarray_get(node->value, i);
+        // printf("[%d] child type = %d, value = %p\n", i, child->type, child->value);
+
+        step(state, child, depth + 1);
+
+        if (i < arr_len - 1) {
+          append_buf_str(state, ", " , 2);
+        }
+      }
+
+      append_buf_str(state, "]", 1);
+      break;
+
+    case NodeString:
+      append_buf_str(state, "\"", 1);
+      append_buf_str(state, node->value, strlen((char*) node->value));
+      append_buf_str(state, "\"", 1);
+      break;
+
+    case NodeLong:
+      sprintf(number_buf, "%ld", *(long*) node->value);
+      append_buf_str(state, number_buf, strlen(number_buf));
+      break;
+
+    case NodeDouble:
+      // TODO preserve original char* for serialisation
+      sprintf(number_buf, "%g", *(double*) node->value);
+      append_buf_str(state, number_buf, strlen(number_buf));
+      break;
+
+    case NodeBool:
+      if (*(int*) node->value == 1) {
+        append_buf_str(state, "true", 4);
+      } else {
+        append_buf_str(state, "false", 5);
+      }
+      break;
+
+    case NodeNull:
+      append_buf_str(state, "null", 4);
+      break;
+  }
+}
+
+char* serialize(node_t* node) {
   serialize_state_t state = {
     .buf = malloc(sizeof(char) * BUF_INCREMENT),
     .buf_pos = 0,
     .buf_len = BUF_INCREMENT
   };
-  char number_buf[1024];
 
-  if (node->type == NodeObject) {
-    append_buf_str(&state, "{", 1);
+  step(&state, node, 0);
 
-    unsigned long obj_key_count = hashmap_count_keys(node->value);
-    char** obj_keys = hashmap_get_keys(node->value);
-    node_t* node_cur;
-
-    for (unsigned long i = 0; i < obj_key_count; i++) {
-      append_buf_str(&state, "\"", 1);
-      append_buf_str(&state, obj_keys[i], strlen(obj_keys[i]));
-      append_buf_str(&state, "\"", 1);
-      append_buf_str(&state, ":", 1);
-
-      node_cur = hashmap_get(node->value, obj_keys[i]);
-
-      switch(node_cur->type) {
-        case NodeString:
-          append_buf_str(&state, "\"", 1);
-          append_buf_str(&state, node_cur->value, strlen((char*) node_cur->value));
-          append_buf_str(&state, "\"", 1);
-          break;
-
-        case NodeLong:
-          sprintf(number_buf, "%ld", *(long*) node_cur->value);
-          append_buf_str(&state, number_buf, strlen(number_buf));
-          break;
-
-        case NodeDouble:
-          // TODO preserve original char* for serialisation
-          sprintf(number_buf, "%g", *(double*) node_cur->value);
-          append_buf_str(&state, number_buf, strlen(number_buf));
-          break;
-
-        case NodeBool:
-          if (*(int*) node_cur->value == 1) {
-            append_buf_str(&state, "true", 4);
-          } else {
-            append_buf_str(&state, "false", 5);
-          }
-          break;
-
-        case NodeNull:
-          append_buf_str(&state, "null", 4);
-          break;
-      }
-
-
-      if (i < obj_key_count - 1) {
-        append_buf_str(&state, ",", 1);
-      }
-    }
-
-    append_buf_str(&state, "}", 1);
-  }
-
-  // terminating the string
   append_buf_str(&state, "\0", 1);
-
   return state.buf;
 }
+
+
+
+// char* serialize_iterative(node_t* node)
+// {
+//   serialize_state_t state = {
+//     .buf = malloc(sizeof(char) * BUF_INCREMENT),
+//     .buf_pos = 0,
+//     .buf_len = BUF_INCREMENT
+//   };
+//   char number_buf[1024];
+
+//   dynarray_t* stack = dynarray_create();
+//   dynarray_t* unclosed = dynarray_create();
+//   dynarray_append(stack, node);
+
+//   while (stack->len > 0) {
+//     // printf("len = %d\n", (int) stack->len);
+//     node_t* cur = dynarray_remove_top(stack);
+//     size_t obj_key_count;
+//     char** obj_keys;
+//     node_t* fake_node = NULL;
+
+//     switch (cur->type) {
+//       case NodeObject:
+//         append_buf_str(&state, "{", 1);
+//         dynarray_append(unclosed, cur);
+
+//         obj_key_count = hashmap_count_keys(cur->value);
+//         obj_keys = hashmap_get_keys(cur->value);
+
+//         for (size_t i = 0; i < obj_key_count; i++) {
+//           // append_buf_str(&state, "\"", 1);
+//           // append_buf_str(&state, obj_keys[i], strlen(obj_keys[i]));
+//           // append_buf_str(&state, "\"", 1);
+//           // append_buf_str(&state, ":", 1);
+
+//           // fake_node = malloc(sizeof(node_t));
+//           // fake_node->type = NodeString;
+//           // fake_node->value = obj_keys[i];
+//           dynarray_append(stack, hashmap_get(cur->value, obj_keys[i]));
+//           // printf("stack len = %d, append type = %d\n", stack->len, ((node_t*) hashmap_get(cur->value, obj_keys[i]))->type);
+//           // dynarray_append(stack, fake_node);
+//         }
+//         break;
+
+//       case NodeArray:
+//         append_buf_str(&state, "[", 1);
+//         dynarray_append(unclosed, cur);
+
+//         for (size_t i = 0; i < ((dynarray_t*) cur->value)->len; i++) {
+//           dynarray_append(stack, dynarray_get(cur->value, i));
+//         }
+//         break;
+
+//       case NodeString:
+//         append_buf_str(&state, "\"", 1);
+//         append_buf_str(&state, cur->value, strlen((char*) cur->value));
+//         append_buf_str(&state, "\"", 1);
+//         break;
+
+//       case NodeLong:
+//         sprintf(number_buf, "%ld", *(long*) cur->value);
+//         append_buf_str(&state, number_buf, strlen(number_buf));
+//         break;
+
+//       case NodeDouble:
+//         // TODO preserve original char* for serialisation
+//         sprintf(number_buf, "%g", *(double*) cur->value);
+//         append_buf_str(&state, number_buf, strlen(number_buf));
+//         break;
+
+//       case NodeBool:
+//         if (*(int*) cur->value == 1) {
+//           append_buf_str(&state, "true", 4);
+//         } else {
+//           append_buf_str(&state, "false", 5);
+//         }
+//         break;
+
+//       case NodeNull:
+//         append_buf_str(&state, "null", 4);
+//         break;
+//     }
+
+//     append_buf_str(&state, ", ", 2);
+//   }
+
+//   while (unclosed->len > 0) {
+//     node_t* cur = dynarray_remove_top(unclosed);
+
+//     switch (cur->type) {
+//       case NodeObject:
+//         append_buf_str(&state, "}", 1);
+//         break;
+
+//       case NodeArray:
+//         append_buf_str(&state, "]", 1);
+//         break;
+//     }
+//   }
+
+//   // terminating the string
+//   append_buf_str(&state, "\0", 1);
+
+//   return state.buf;
+// }
--- serialize.h
+++ serialize.h
@@ -1,8 +1,14 @@
 #ifndef SERIALIZE_H
 #define SERIALIZE_H
 
+#include <stdlib.h>
+#include <string.h>
 #include <stddef.h>
+
 #include "parse.h"
+#include "dynarray.h"
+#include "hashmap.h"
+#include "util.h"
 
 char* serialize(node_t* node);
 
